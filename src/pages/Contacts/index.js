fdb4c84b142cb7932fd41d3bd1530956374d3109
TUMEN ZORIKTUEV <69411964+TMNZKTV@users.noreply.github.com>
2021-09-24 19:32:54 +0800

    custom hook and new structure

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,44 @@
+import { useState, useEffect } from "react";
+
+const useContacts = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch("https://randomuser.me/api/?results=200");
+        const { results, error } = await response.json();
+        if (error) {
+          throw new Error(error);
+        }
+        setData(results);
+        setIsError(false);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getContacts();
+  }, []);
+
+  return {
+    data,
+    isLoading,
+    isError,
+  };
+};
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  const contacts = useContacts();
+  if (contacts.isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (contacts.isError) {
+    return <div>Error!</div>;
+  }
+
+  return <div>Contacts {contacts.data[0].name.first}</div>;
 };
